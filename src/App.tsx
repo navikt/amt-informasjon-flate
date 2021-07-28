7767a976cb265586a00befa041d6b502d732565b
André Schjøth <andre.schjoth@nav.no>
2021-07-28 15:53:52 +0200

    tc-69 stylet tiltaksboks

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,18 @@ import TiltakOversikt from './components/tiltaksoversikt/TiltakOversikt';
 import './App.less';
 
 function App() {
+  //TODO fjern når vi får koblet til backend
   const tiltaksliste = [
-    { id: 1, tittel: 'Tittel1', beskrivelse: 'Lorem Ipsum dolor sit amet' },
-    { id: 2, tittel: 'Tittel2', beskrivelse: 'Lorem Ipsum dolor sit amet' },
+    { id: 1, tittel: 'Tittel', ingress: 'Lorem Ipsum dolor sit amet' },
+    { id: 2, tittel: 'Tittel', ingress: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+    { id: 3, tittel: 'Tittel', ingress: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim.' },
+    { id: 4, tittel: 'Tittel', ingress: 'Lorem Ipsum dolor sit amet' },
+    { id: 5, tittel: 'Tittel', ingress: 'Lorem Ipsum dolor sit amet' },
+    {
+      id: 6,
+      tittel: 'Tittel',
+      ingress: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
   ];
 
   return (
--- src/components/tiltaksoversikt/Tiltaksboks.tsx
+++ src/components/tiltaksoversikt/Tiltaksboks.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import './Tiltak.less';
+import '../../App.less';
+import { Undertittel } from 'nav-frontend-typografi';
 
 export interface TiltaksboksProps {
   id: number;
-  tittel: String;
-  beskrivelse: String;
+  tittel: string;
+  ingress: string;
+  bilde?: Blob;
 }
 
 const Tiltaksboks = (props: TiltaksboksProps) => {
+  //TODO: legge til et grunnleggende bilde?
+  const imgUrl = props.bilde ? URL.createObjectURL(props.bilde) : '';
+
   return (
-    <a href="">
-      {/* Lenke til visning av tiltaket */}
+    <a href="" className="remove_link_style">
+      {/* TODO: legg til lenke for visning av tiltaket */}
 
-      <div className="tiltakBox">
-        {/* Kanskje bytte ut med nav komponent? */}
-        <p>
-          <b>{props.tittel}</b>
-        </p>
+      <div className="tiltakboks">
+        <img src={imgUrl} alt={props.tittel} />
+        <div>
+          <Undertittel className="tittel">{props.tittel}</Undertittel>
 
-        <p>{props.beskrivelse}</p>
+          <p>{props.ingress}</p>
+        </div>
       </div>
     </a>
   );
