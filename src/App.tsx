2b34ce4f5d486d06d5d5ab34cbca895121d197fa
taniaholst <tania.holst@nav.no>
2021-07-28 10:09:40 +0200

    tc-68 satt opp redux og redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Decorator from './decorator/decorator';
+import Decorator from './decorator/Decorator';
 
 function App() {
   return (
     <>
       <Decorator />
+      <div className="main-app"></div>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import store from './redux/Store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('arbeidstiltak-visning-root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('arbeidstiltak-visning-root')
+);
