19c18dd40331b96d0bad4d4b6f66c056d83c05b0
André Schjøth <andre.schjoth@nav.no>
2021-07-30 12:33:26 +0200

    tc-71 lagde gjenbrukbar dropdown komponent med checkboxes

---
+++ src/components/filtrering/DropdownFilter.tsx
@@ -0,0 +1,34 @@
+import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
+import { Checkbox } from 'nav-frontend-skjema';
+import React from 'react';
+
+interface DropDownFilterProps {
+  filtreringsmuligheter: Array<string>;
+  onChange: (filter: string) => void;
+  tittel: string;
+}
+
+const DropDownFilter = ({ filtreringsmuligheter, onChange, tittel }: DropDownFilterProps) => {
+  return (
+    <>
+      <Ekspanderbartpanel
+        renderContentWhenClosed={true}
+        tittel={tittel}
+        className="dropdownFilter"
+        style={{ background: 'rgba(179, 193, 0, 0.4)' }}
+      >
+        {filtreringsmuligheter.map((filtreringsmulighet, index) => (
+          <Checkbox
+            label={filtreringsmulighet}
+            value={filtreringsmulighet}
+            key={index}
+            className="dropdownCheckbox"
+            onChange={() => onChange(filtreringsmulighet)}
+          />
+        ))}
+      </Ekspanderbartpanel>
+    </>
+  );
+};
+
+export default DropDownFilter;
