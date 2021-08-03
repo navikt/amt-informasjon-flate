2bf1ea679ac33d120976fe83038dd35d778a3279
Tania Holst <holst.tania@gmail.com>
2021-08-03 10:16:46 +0200

    cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: ['plugin:react/recommended', 'standard', 'prettier'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    plugins: ['react', '@typescript-eslint', 'prettier'],
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
 
-    rules: {
-        '@typescript-eslint/no-use-before-define': ['off'],
-        'no-use-before-define': 'off',
-        'space-before-function-paren': ['off'],
-        'prettier/prettier': ['warn'],
-        'no-unused-vars': ['warn'],
-        'no-useless-return': ['off'],
-        'spaced-comment': ['off'],
-    },
+  rules: {
+    '@typescript-eslint/no-use-before-define': ['off'],
+    'no-use-before-define': 'off',
+    'space-before-function-paren': ['off'],
+    'prettier/prettier': ['warn'],
+    'no-unused-vars': ['warn'],
+    'no-useless-return': ['off'],
+    'spaced-comment': ['off'],
+  },
 };
