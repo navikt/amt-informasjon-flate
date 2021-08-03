6ce8dd047ebb08cfcb465371950aa5a2f05066b5
André Schjøth <andre.schjoth@nav.no>
2021-08-03 10:16:41 +0200

    tc-72 fjernet ubrukt import

--- src/components/filtrering/RegionFilter.tsx
+++ src/components/filtrering/RegionFilter.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import { Checkbox } from 'nav-frontend-skjema';
-import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleRegionFilter } from '../../redux/filtreringer/StedFilterSlice';
 import tilhorendeKommuner from '../../data/geografi';
