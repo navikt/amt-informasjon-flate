5bea1934771680bc217dbdff5e4cf9cfb4d0f914
André Schjøth <andre.schjoth@nav.no>
2021-08-11 12:14:23 +0200

    tc-113 fjerne noe dummy data

--- src/components/tiltaksvisning/Tiltaksvisning.tsx
+++ src/components/tiltaksvisning/Tiltaksvisning.tsx
@@ -9,6 +9,7 @@ import { Innholdstittel, Undertittel } from 'nav-frontend-typografi';
 import { useSelector } from 'react-redux';
 import { Tilbakeknapp } from 'nav-frontend-ikonknapper';
 import { useQuery } from 'react-query';
+import NavFrontendSpinner from 'nav-frontend-spinner';
 
 interface routeParams {
   id: string;
@@ -26,79 +27,43 @@ const Tiltaksvisning = () => {
 
   const { id }: routeParams = useParams();
 
-  const { isLoading, data } = useQuery('tiltak' + id, () =>
+  const { isLoading, data, error } = useQuery('tiltak' + id, () =>
     fetch(process.env.REACT_APP_BACKEND_API_ROOT + '/api/tiltak/' + id).then(res => res.json())
   );
 
-  console.log(isLoading, data);
+  const faner = Array.from(data?.faner.keys());
 
-  const tiltak = {
-    id,
-    tittel: 'Truckførerkurs',
-    ingress:
-      'Truckførerkurset gir grunnleggende sikkerhetsopplæring for førere av truck. Opplæringen er todelt, med én teoretisk del og én praktisk del, og er delt opp i fire moduler.',
-    beskrivelse:
-      'Modul 1, 2 og 3 må gjennomføres av oss som opplæringsvirksomhet, mens modul 4 kan gjennomføres enten som bedriftsintern opplæring, eller gjennom Kurs og Sikkerhet. Se beskrivelse under for mer utfyllende informasjon og et priseksempel.',
-    bilde:
-      'https://assets.website-files.com/5f0454ca439d52fef0fb4143/600e78d9b3db13ded972087f_shutterstock_1075806023-p-1600.jpeg',
-    faner: new Map<string, Array<string>>([
-      [
-        'Formål og innhold',
-        [
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec iaculis lacus. Sed facilisis risus eu elit condimentum porttitor eu a eros. Fusce ut interdum justo.',
-          'Suspendisse id est erat. Morbi non euismod diam. Pellentesque eu gravida diam. Cras ultricies ipsum maximus tempus fringilla.',
-          'Pellentesque sagittis ligula nisi, et mollis neque eleifend in. Phasellus nec velit a dui convallis hendrerit et aliquam diam. Aenean ut dui sollicitudin, malesuada risus vel, feugiat nulla.',
-        ],
-      ],
-      [
-        'Opptakskrav',
-        [
-          'Suspendisse id est erat. Morbi non euismod diam. Pellentesque eu gravida diam. Cras ultricies ipsum maximus tempus fringilla.',
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec iaculis lacus. Sed facilisis risus eu elit condimentum porttitor eu a eros. Fusce ut interdum justo.',
-          'Vivamus metus massa, vehicula ac euismod non, dapibus imperdiet sapien. Nullam id viverra neque. Fusce ut justo volutpat, consequat lacus a, elementum dolor. Aenean vel pellentesque massa. Fusce accumsan erat at blandit imperdiet. Praesent convallis nisl est, vel finibus justo commodo id.',
-        ],
-      ],
-      [
-        'Tid og sted',
-        [
-          'Suspendisse id est erat. Morbi non euismod diam. Pellentesque eu gravida diam. Cras ultricies ipsum maximus tempus fringilla.',
-          'Vivamus metus massa, vehicula ac euismod non, dapibus imperdiet sapien. Nullam id viverra neque. Fusce ut justo volutpat, consequat lacus a, elementum dolor. Aenean vel pellentesque massa. Fusce accumsan erat at blandit imperdiet. Praesent convallis nisl est, vel finibus justo commodo id.',
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec iaculis lacus. Sed facilisis risus eu elit condimentum porttitor eu a eros. Fusce ut interdum justo.',
-        ],
-      ],
-      [
-        'Øvrig informasjon',
-        [
-          'Vivamus metus massa, vehicula ac euismod non, dapibus imperdiet sapien. Nullam id viverra neque. Fusce ut justo volutpat, consequat lacus a, elementum dolor. Aenean vel pellentesque massa. Fusce accumsan erat at blandit imperdiet. Praesent convallis nisl est, vel finibus justo commodo id.',
-          'Suspendisse id est erat. Morbi non euismod diam. Pellentesque eu gravida diam. Cras ultricies ipsum maximus tempus fringilla.',
-          'Pellentesque sagittis ligula nisi, et mollis neque eleifend in. Phasellus nec velit a dui convallis hendrerit et aliquam diam. Aenean ut dui sollicitudin, malesuada risus vel, feugiat nulla.',
-        ],
-      ],
-    ]),
-  };
+  const tabs: TabProps[] = data?.faner.map((fane: String) => ({ label: fane }));
 
-  const faner = Array.from(tiltak.faner.keys());
-
-  const tabs: TabProps[] = faner.map(fane => ({ label: fane }));
-
-  const [aktivFaneBeskrivelse, setAktivFaneBeskrivelse] = useState<Array<string>>(tiltak.faner.get(faner[0]) || []);
+  const [aktivFaneBeskrivelse, setAktivFaneBeskrivelse] = useState<Array<string>>(data?.faner.get(faner[0]) || []);
 
   const endreInnholdsvisning = (e: SyntheticEvent<EventTarget, Event>, index: number) => {
-    setAktivFaneBeskrivelse(tiltak.faner.get(faner[index]) || []);
+    setAktivFaneBeskrivelse(data?.faner.get(faner[index]) || []);
   };
 
+  data.bilde =
+    'https://assets.website-files.com/5f0454ca439d52fef0fb4143/600e78d9b3db13ded972087f_shutterstock_1075806023-p-1600.jpeg';
+
   const history = useHistory();
+
+  if (isLoading) {
+    return <NavFrontendSpinner />;
+  }
+
+  if (error) {
+    return <p>Det skjedde en feil</p>;
+  }
 
   return (
     <>
       <div className="tiltaksvisning__grid">
         <Tilbakeknapp className="tilbakeknapp" onClick={() => history.push('/')} />
 
-        {brukervisningsToggle && <Undertittel className="tiltaksnummer">{`Tiltaksnummer: ${tiltak.id}`}</Undertittel>}
+        {brukervisningsToggle && <Undertittel className="tiltaksnummer">{`Tiltaksnummer: ${data?.id}`}</Undertittel>}
         <BrukerVisningsToggle className="tiltaksvisning__brukervisningstoggle info__brukervisningstoggle" />
 
         <div className="generell-informasjon">
-          <img src={tiltak.bilde} alt={'bilde av ' + data?.tittel} />
+          <img src={data?.bilde} alt={'bilde av ' + data?.tittel} />
 
           <div className="beskrivelse">
             <Innholdstittel>{data?.tittel}</Innholdstittel>
