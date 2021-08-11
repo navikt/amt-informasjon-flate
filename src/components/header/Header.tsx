c5e62cf218376e52a8221566cca75e2108bfcf20
André Schjøth <andre.schjoth@nav.no>
2021-08-11 10:44:57 +0200

    tc-112 byttet til react router sin component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { ReactComponent as Logo } from '../../ikoner/MulighetsrommetLogo.svg';
 import { Sidetittel, Undertittel } from 'nav-frontend-typografi';
 import './Header.less';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="header__border">
       <div className="header">
-        <a href="/" aria-label="Gå til forsiden" className="header__logo">
+        <Link to="/" aria-label="Gå til forsiden" className="header__logo">
           <Logo />
-        </a>
+        </Link>
+
         <Sidetittel className="header__tittel">Mulighetsrommet</Sidetittel>
         <Undertittel className="header__undertittel">NAV tilbyr et hav av muligheter</Undertittel>
       </div>
