ed4228e499f3ba30195155ce8aed43eef2d5e72a
taniaholst <tania.holst@nav.no>
2021-09-20 08:47:25 +0200

    tc-142 fikset toggle

--- src/components/body/TiltakOgFilterOversikt.tsx
+++ src/components/body/TiltakOgFilterOversikt.tsx
@@ -2,14 +2,24 @@ import React from 'react';
 import Tiltaksoversikt from '../tiltaksoversikt/Tiltaksoversikt';
 import './TiltakOgFilterOversikt.less';
 import Sokefelt from '../filtrering/Sokefelt';
-import BildeListeVisningsToggle from '../toggle/BildeListeVisningsToggle';
 import TiltakstypeFilter from '../filtrering/dropdowns/TiltakstypeFilter';
+import { toggleBildeListeVisning } from '../../redux/slice/ToggleSlice';
+import Toggle from '../toggle/Toggle';
+import { useSelector } from 'react-redux';
 
 const TiltakOgFilterOversikt = () => {
+  const bildeListeToggle = useSelector((state: any) => state.toggleReducer.bildeListeVisning);
+
   return (
     <div className="tiltak-og-filter__grid">
       <Sokefelt />
-      <BildeListeVisningsToggle />
+      <Toggle
+        toggle={bildeListeToggle}
+        onChange={toggleBildeListeVisning}
+        venstreTekst="Bildevisning"
+        hoyreTekst="Listevisning"
+        className="bildelistevisning__toggle"
+      />
       <TiltakstypeFilter />
       <Tiltaksoversikt />
     </div>
