b7bb886b43c78b2a8f67860006cd1e48594bc657
Steffen Lien <steffen.lien@nav.no>
2021-10-01 10:58:06 +0200

    Legg til content-type

--- src/core/api/ApiUtils.ts
+++ src/core/api/ApiUtils.ts
@@ -1,7 +1,11 @@
 // eslint-disable-next-line no-undef
 export function api<T>(input: RequestInfo, init?: RequestInit | undefined): Promise<T> {
   const uri = `${process.env.REACT_APP_BACKEND_API_ROOT}/api${input}`;
-  return fetch(uri, init)
+  const rInit = { ...init };
+  rInit.headers = {
+    'Content-Type': 'application/json',
+  };
+  return fetch(uri, rInit)
     .then(response => {
       if (!response.ok) {
         throw new Error(response.statusText);
