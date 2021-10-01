1fdbc231210592c12431dbd9e30eada7ca66e362
trudejostad <trude.jostad@nav.no>
2021-10-01 11:38:43 +0200

    bare bruke listevisning og ikke bilde

--- src/views/tiltakstype-oversikt/TiltakOgFilterOversikt.tsx
+++ src/views/tiltakstype-oversikt/TiltakOgFilterOversikt.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import Tiltaksoversikt from '../../components/tiltaksoversikt/Tiltaksoversikt';
 import Sokefelt from '../../components/filtrering/Sokefelt';
-import { toggleBildeListeVisning } from '../../core/redux/slice/ToggleSlice';
-import Toggle from '../../components/toggle/Toggle';
 import { Knapp } from 'nav-frontend-knapper';
 import Lenke from 'nav-frontend-lenker';
 import './TiltakOgFilterOversikt.less';
 
 const TiltakOgFilterOversikt = () => {
-  const bildeListeToggle = useSelector((state: any) => state.toggleReducer.bildeListeVisning);
 
   return (
     <div className="tiltak-og-filter__grid">
@@ -18,13 +14,6 @@ const TiltakOgFilterOversikt = () => {
       <Lenke href="../admin/opprett-tiltakstype" className="opprett-nytt-tiltak__knapp">
         <Knapp>Opprett ny tiltakstype</Knapp>
       </Lenke>
-      <Toggle
-        toggle={bildeListeToggle}
-        onChange={toggleBildeListeVisning}
-        venstreTekst="Bildevisning"
-        hoyreTekst="Listevisning"
-        className="bildelistevisning__toggle"
-      />
       <Tiltaksoversikt />
     </div>
   );
