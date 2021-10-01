3ccb6fdd2f98faab3c17f3a05c485d770114cab7
Steffen Lien <steffen.lien@nav.no>
2021-10-01 11:56:12 +0200

    Fjern igjenglemt Lenke

--- src/views/tiltakstype-oversikt/TiltakOgFilterOversikt.tsx
+++ src/views/tiltakstype-oversikt/TiltakOgFilterOversikt.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Tiltaksoversikt from '../../components/tiltaksoversikt/Tiltaksoversikt';
 import Sokefelt from '../../components/filtrering/Sokefelt';
 import { Knapp } from 'nav-frontend-knapper';
-import Lenke from 'nav-frontend-lenker';
 import './TiltakOgFilterOversikt.less';
 import { useQuery } from 'react-query';
 import { QueryKeys } from '../../core/api/QueryKeys';
 import TiltakstypeService from '../../core/api/TiltakstypeService';
 import { Tiltakstype } from '../../core/domain/Tiltakstype';
+import { Link } from 'react-router-dom';
 
 const TiltakOgFilterOversikt = () => {
   const { isLoading, isSuccess, data, isError } = useQuery<Tiltakstype[]>(
@@ -19,9 +19,9 @@ const TiltakOgFilterOversikt = () => {
     <div className="tiltak-og-filter__grid">
       <Sokefelt />
 
-      <Lenke href="../admin/opprett-tiltakstype" className="opprett-nytt-tiltak__knapp">
+      <Link to="/admin/opprett-tiltakstype" className="opprett-nytt-tiltak__knapp">
         <Knapp>Opprett ny tiltakstype</Knapp>
-      </Lenke>
+      </Link>
       <Tiltaksoversikt tiltakstyper={data} />
     </div>
   );
