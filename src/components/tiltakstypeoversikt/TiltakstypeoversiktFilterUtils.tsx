c2f7b34ff4a76354f60caa0f96463778017c86c4
trudejostad <trude.jostad@nav.no>
2021-10-12 12:29:38 +0200

    sletter ubruk fil

--- src/components/tiltakstypeoversikt/TiltakstypeoversiktFilterUtils.tsx
+++
@@ -1,8 +0,0 @@
-import { Tiltakstype } from '../../core/domain/Tiltakstype';
-
-export const isSearchTextInFilter = (tiltakstype: Tiltakstype, searchText: string) => {
-  return (
-    tiltakstype.tittel.toLowerCase().includes(searchText.toLowerCase()) ||
-    tiltakstype.ingress.toLowerCase().includes(searchText.toLowerCase())
-  );
-};
