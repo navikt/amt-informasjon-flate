ff50ad04c50c006ec79fd9fa257173160027cd51
taniaholst <tania.holst@nav.no>
2021-10-26 10:07:30 +0200

    tc-241 merge dev

--- src/layouts/MainView.tsx
+++ src/layouts/MainView.tsx
@@ -1,9 +1,9 @@
 import React, { FunctionComponent } from 'react';
-import { Container, Row } from 'react-bootstrap';
 import { Tilbakeknapp } from 'nav-frontend-ikonknapper';
 import './MainView.less';
 import Link from '../components/link/Link';
 import MainViewTitle from './MainViewTitle';
+import { Container, Row } from 'react-bootstrap';
 
 interface MainViewProps {
   showBackButton?: boolean;
@@ -13,9 +13,20 @@ interface MainViewProps {
   tilbakelenke?: string;
 }
 
-const MainView: FunctionComponent<MainViewProps> = ({ showBackButton, title, subTitle, dataTestId, tilbakelenke, children }) => {
+const MainView: FunctionComponent<MainViewProps> = ({
+  showBackButton,
+  title,
+  subTitle,
+  dataTestId,
+  tilbakelenke = '/',
+  children,
+}) => {
   const renderTitleSection = () =>
     title ? <MainViewTitle title={title} subTitle={subTitle} dataTestId={dataTestId} /> : null;
+  return (
+    <Container className="main-view">
+      <Row className="main-view__header">
+        {showBackButton && (
           <Link to={tilbakelenke}>
             <Tilbakeknapp data-testid="tilbakeknapp" />
           </Link>
