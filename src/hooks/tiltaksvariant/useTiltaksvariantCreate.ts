0bf15e137123720332c7759f0653752d07e4382a
Sondre Lefsaker <sondre.lefsaker@nav.no>
2021-11-26 15:02:38 +0100

    midlertidig fjern kode relatert til optimistic updates
    Ref diskusjon

--- src/hooks/tiltaksvariant/useTiltaksvariantCreate.ts
+++ src/hooks/tiltaksvariant/useTiltaksvariantCreate.ts
@@ -1,29 +1,20 @@
-import { useMutation, useQueryClient } from 'react-query';
+import { useMutation } from 'react-query';
 import { useHistory } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { QueryKeys } from '../../core/api/QueryKeys';
 import TiltaksvariantService from '../../core/api/TiltaksvariantService';
 import { Tiltaksvariant } from '../../core/domain/Tiltaksvariant';
 
 export default function useTiltaksvariantCreate() {
   const history = useHistory();
-  const queryClient = useQueryClient();
-  return useMutation(
-    QueryKeys.Tiltaksvarianter,
-    (tiltaksvariant: Tiltaksvariant) => TiltaksvariantService.postTiltaksvariant(tiltaksvariant),
-    {
-      onMutate: async () => {
-        await queryClient.cancelQueries(QueryKeys.Tiltaksvarianter);
-      },
-      onSettled: (newTiltaksvariant, error) => {
-        if (error) {
-          toast.error('Oppretting feilet.');
-        } else {
-          newTiltaksvariant && queryClient.invalidateQueries(QueryKeys.Tiltaksvarianter);
-          toast.success('Oppretting vellykket!');
-          history.replace(`/tiltaksvarianter/${newTiltaksvariant?.id}`);
-        }
-      },
-    }
-  );
+
+  return useMutation((tiltaksvariant: Tiltaksvariant) => TiltaksvariantService.postTiltaksvariant(tiltaksvariant), {
+    onSuccess(tiltaksvariant) {
+      toast.success('Oppretting vellykket!');
+
+      history.replace(`/tiltaksvarianter/${tiltaksvariant.id}`);
+    },
+    onError() {
+      toast.error('Oppretting feilet.');
+    },
+  });
 }
--- src/hooks/tiltaksvariant/useTiltaksvariantDelete.ts
+++ src/hooks/tiltaksvariant/useTiltaksvariantDelete.ts
@@ -1,27 +1,20 @@
-import { useMutation, useQueryClient } from 'react-query';
+import { useMutation } from 'react-query';
 import { useHistory } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { QueryKeys } from '../../core/api/QueryKeys';
 import TiltaksvariantService from '../../core/api/TiltaksvariantService';
 import { Id } from '../../core/domain/Generic';
 
 export default function useTiltaksvariantDelete(id: Id) {
   const history = useHistory();
-  const queryClient = useQueryClient();
+
   return useMutation(() => TiltaksvariantService.deleteTiltaksvariant(id), {
-    onMutate: async () => {
-      await queryClient.cancelQueries(QueryKeys.Tiltaksvarianter);
-      const prevTiltaksvarianter = queryClient.getQueryData(QueryKeys.Tiltaksvarianter);
-      return { prevTiltaksvarianter };
+    onSuccess() {
+      toast.success('Sletting vellykket!');
+
+      history.replace(`/`);
     },
-    onSettled: (tiltaksvariant, error, variables, context: any) => {
-      if (error) {
-        queryClient.setQueryData(QueryKeys.Tiltaksvarianter, context.prevTiltaksvarianter);
-        toast.error('Sletting feilet.');
-      } else {
-        toast.success('Sletting vellykket!');
-        history.replace(`/`);
-      }
+    onError() {
+      toast.error('Sletting feilet.');
     },
   });
 }
